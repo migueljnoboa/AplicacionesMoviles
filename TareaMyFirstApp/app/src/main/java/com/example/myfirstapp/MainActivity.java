4e15acde8bca22e910a5873f50a47fc17336ac05
migueljnoboa <20190327@ce.pucmm.edu.do>
2023-09-20 23:06:33 -0400

    Tarea Completada, todo funciona correctamente

--- TareaMyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ TareaMyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.myfirstapp;
 
+import static android.app.PendingIntent.getActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.res.XmlResourceParser;
 import android.os.Bundle;
@@ -9,11 +13,15 @@ import android.util.Xml;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,14 +29,54 @@ public class MainActivity extends AppCompatActivity {
     public static final String RESPONSE2 = "com.example.myfirstapp.RESPONSE2";
     public static final String RESPONSE3 = "com.example.myfirstapp.RESPONSE3";
 
+    private EditText inputNombre;
+    private EditText inputApellido;
+    private Spinner spinnerGenero;
+    private Button buttonNacimiento;
+    private RadioButton radioSi;
+    private  RadioButton radioNo;
+    private CheckBox java;
+    private CheckBox python;
+    private CheckBox JS;
+    private CheckBox Go;
+    private CheckBox C;
+    private CheckBox CSharp;
+
+    private DatePickerDialog datePickerDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void openDatePicker(View view) {
-        // TODO: open DatePicker
+        inputNombre = (EditText) findViewById(R.id.inputNombre);
+        inputApellido = (EditText) findViewById(R.id.inputApellido);
+        spinnerGenero = (Spinner) findViewById(R.id.spinnerGenero);
+        buttonNacimiento = (Button) findViewById(R.id.buttonNacimiento);
+        radioSi = findViewById(R.id.radioSi);
+        radioNo = (RadioButton) findViewById(R.id.radioNo);
+        java = (CheckBox) findViewById(R.id.checkJava);
+        python = (CheckBox) findViewById(R.id.checkPython);
+        JS = (CheckBox) findViewById(R.id.checkJS);
+        Go = (CheckBox) findViewById(R.id.checkGo);
+        C = (CheckBox) findViewById(R.id.checkC);
+        CSharp = (CheckBox) findViewById(R.id.checkCSharp);
+
+
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                String date = day + "/" + month + "/" + year;
+                buttonNacimiento.setText(date);
+            }
+        };
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        datePickerDialog = new DatePickerDialog(this, dateSetListener, year, month, day);
+
     }
 
     // send the information from activity_main to activity_response and activate activity_response
@@ -47,27 +95,12 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, ResponseActivity.class);
 
-        // Creating Response 1
-        EditText inputNombre = (EditText) findViewById(R.id.inputNombre);
-        EditText inputApellido = (EditText) findViewById(R.id.inputApellido);
-
         String response1 = "Hola!, mi nombre es: " + inputNombre.getText().toString() + " " + inputApellido.getText().toString() + ".";
-
-        // Creating Response 2
-        Spinner spinnerGenero = (Spinner) findViewById(R.id.spinnerGenero);
-        Button buttonNacimiento = (Button) findViewById(R.id.buttonNacimiento);
 
         String response2 = "Soy " + spinnerGenero.getSelectedItem().toString() + ", y naci en fecha " + buttonNacimiento.getText().toString() + ".";
 
         // Creating Response 3
         String response3 = "";
-        RadioButton radioNo = (RadioButton) findViewById(R.id.radioNo);
-        CheckBox java = (CheckBox) findViewById(R.id.checkJava);
-        CheckBox python = (CheckBox) findViewById(R.id.checkPython);
-        CheckBox JS = (CheckBox) findViewById(R.id.checkJS);
-        CheckBox Go = (CheckBox) findViewById(R.id.checkGo);
-        CheckBox C = (CheckBox) findViewById(R.id.checkC);
-        CheckBox CSharp = (CheckBox) findViewById(R.id.checkCSharp);
 
         if (radioNo.isChecked()){
             response3 = "No me gusta programar.";
@@ -111,41 +144,35 @@ public class MainActivity extends AppCompatActivity {
         boolean check = true;
 
         // Check if Nombre is Empty
-        EditText inputNombre = (EditText) findViewById(R.id.inputNombre);
         if(inputNombre.getText().toString().equalsIgnoreCase("")){
-            //TODO: Add dialog for Nombre
+
+            Toast.makeText(this, "Nombre Invalido", Toast.LENGTH_SHORT).show();
             return false;
         }
 
         // Check if Apellido is Empty
-        EditText inputApellido = (EditText) findViewById(R.id.inputApellido);
         if(inputApellido.getText().toString().equalsIgnoreCase("")){
-            //TODO: add dialog for Apellido
+
+            Toast.makeText(this, "Apellido Invalido", Toast.LENGTH_SHORT).show();
             return false;
         }
 
         // Check if Genero is at 0
-        Spinner spinnerGenero = (Spinner) findViewById(R.id.spinnerGenero);
         if (spinnerGenero.getSelectedItemPosition() == 0){
-            //TODO: add dialog for Genero
+
+            Toast.makeText(this, "Genero Invalido", Toast.LENGTH_SHORT).show();
             return false;
         }
 
         // Check if a Date for Nacimiento has been picked or if its default
-        /*Button buttonNacimiento = (Button) findViewById(R.id.buttonNacimiento);
-        if (buttonNacimiento.getText().toString().equalsIgnoreCase("Fecha de Nacimiento")){
-            //TODO: add dialog for Nacimiento
+        Button buttonNacimiento = (Button) findViewById(R.id.buttonNacimiento);
+        if (buttonNacimiento.getText().toString().equalsIgnoreCase(this.getString(R.string.defaultDate))){
+
+            Toast.makeText(this, "Fecha Invalido", Toast.LENGTH_SHORT).show();
             return false;
-        }*/
+        }
 
         // Check if "Le Gusta Programar" is set Right
-        CheckBox java = (CheckBox) findViewById(R.id.checkJava);
-        CheckBox python = (CheckBox) findViewById(R.id.checkPython);
-        CheckBox JS = (CheckBox) findViewById(R.id.checkJS);
-        CheckBox Go = (CheckBox) findViewById(R.id.checkGo);
-        CheckBox C = (CheckBox) findViewById(R.id.checkC);
-        CheckBox CSharp = (CheckBox) findViewById(R.id.checkCSharp);
-        RadioButton radioNo = (RadioButton) findViewById(R.id.radioNo);
         if (radioNo.isChecked() &&
                 (java.isChecked()
                 || python.isChecked()
@@ -153,7 +180,8 @@ public class MainActivity extends AppCompatActivity {
                 || Go.isChecked()
                 || C.isChecked()
                 || CSharp.isChecked())){
-            // TODO: add dialog for Programar
+
+            Toast.makeText(this, "Si no te gusta programar, no puedes tener lenguajes favoritos.", Toast.LENGTH_SHORT).show();
             return false;
         }
         return true;
@@ -163,32 +191,21 @@ public class MainActivity extends AppCompatActivity {
     public void cleanForm(View view) {
 
         // Clean Nombre
-        EditText inputNombre = (EditText) findViewById(R.id.inputNombre);
         inputNombre.setText("");
 
         // Clean Apellido
-        EditText inputApellido = (EditText) findViewById(R.id.inputApellido);
         inputApellido.setText("");
 
         // Clean Genero
-        Spinner spinnerGenero = (Spinner) findViewById(R.id.spinnerGenero);
         spinnerGenero.setSelection(0);
 
         // Clean Nacimiento
-        Button buttonNacimiento = (Button) findViewById(R.id.buttonNacimiento);
         buttonNacimiento.setText("Fecha de Nacimiento");
 
         // Clean Radio Group
-        RadioButton radioSi = findViewById(R.id.radioSi);
         radioSi.setChecked(true);
 
         // Clean Check Boxes
-        CheckBox java = (CheckBox) findViewById(R.id.checkJava);
-        CheckBox python = (CheckBox) findViewById(R.id.checkPython);
-        CheckBox JS = (CheckBox) findViewById(R.id.checkJS);
-        CheckBox Go = (CheckBox) findViewById(R.id.checkGo);
-        CheckBox C = (CheckBox) findViewById(R.id.checkC);
-        CheckBox CSharp = (CheckBox) findViewById(R.id.checkCSharp);
         java.setChecked(false);
         python.setChecked(false);
         JS.setChecked(false);
@@ -196,5 +213,9 @@ public class MainActivity extends AppCompatActivity {
         C.setChecked(false);
         CSharp.setChecked(false);
 
+    }
+
+    public void openCalendar(View view) {
+        datePickerDialog.show();
     }
 }
--- TareaMyFirstApp/app/src/main/java/com/example/myfirstapp/ResponseActivity.java
+++ TareaMyFirstApp/app/src/main/java/com/example/myfirstapp/ResponseActivity.java
@@ -8,19 +8,23 @@ import android.widget.TextView;
 
 public class ResponseActivity extends AppCompatActivity {
 
+    TextView textResponse1;
+    TextView textResponse2;
+    TextView textResponse3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_response);
 
         Intent intent = getIntent();
+        textResponse1 = findViewById(R.id.textResponse1);
+        textResponse2 = findViewById(R.id.textResponse2);
+        textResponse3 = findViewById(R.id.textResponse3);
+
         String response1 = intent.getStringExtra(MainActivity.RESPONSE1);
         String response2 = intent.getStringExtra(MainActivity.RESPONSE2);
         String response3 = intent.getStringExtra(MainActivity.RESPONSE3);
-
-        TextView textResponse1 = findViewById(R.id.textResponse1);
-        TextView textResponse2 = findViewById(R.id.textResponse2);
-        TextView textResponse3 = findViewById(R.id.textResponse3);
 
         textResponse1.setText(response1);
         textResponse2.setText(response2);
